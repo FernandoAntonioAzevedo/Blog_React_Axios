fecd72fb910e254539f6b8904099131bc96eee3d
FernandoAntonioAzevedo <beratechsolutions@gmail.com>
2024-08-09 16:24:43 -0300

    iniciando estrutura do projeto

--- react-axios/src/main.jsx
+++ react-axios/src/main.jsx
@@ -1,10 +1,27 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React, { Children } from "react";
+import ReactDOM from "react-som/client";
+import App from "./App";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
+import {createBrowserRoute, RouterProvider, Route} from "react-router-dom";
+
+import "./index.css";
+
+const router = createBrowserRoute([
+  {
+    element: <App />,
+    children: [
+      {
+        path: "/",
+      },
+      {
+        path: "/new",
+      },
+    ],
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
     <App />
-  </StrictMode>,
-)
+  </React.StrictMode>
+);
