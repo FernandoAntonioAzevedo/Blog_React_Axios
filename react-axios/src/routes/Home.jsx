c14831feb98ecbd492fbbf6975f20917b315009f
FernandoAntonioAzevedo <beratechsolutions@gmail.com>
2024-08-13 14:08:45 -0300

    fizando estilos da Home e alinhando base URL no Axios

--- react-axios/src/routes/Home.jsx
+++ react-axios/src/routes/Home.jsx
@@ -7,20 +7,17 @@ import { Link } from "react-router-dom";
 import "./Home.css";
 
 const Home = () => {
-
   const [posts, setPosts] = useState ([]);
 
   const getPosts = async() => {
-
     try {
-
       const response = await axios.get(
         "https://jsonplaceholder.typicode.com/posts"
       );     
 
       const data = response.data;
 
-      console.log(data);
+      setPosts(data);
     } catch (error) {
       console.log(error);
       
@@ -33,7 +30,22 @@ const Home = () => {
   }, [])
 
 
-  return <div>Home</div>;  
+  return (
+  <div className="home">
+    <h1>Últimos Posts</h1>
+    {posts.length === 0 ? (<p>Carregando...</p>) : (
+      posts.map((post) => (
+        <div className="post" key={post.id}>
+          <h2>{post.title}</h2>
+          <p>{post.body}</p>
+          <Link to={`/posts/${post.id}`} className="btn">
+          Ler mais
+          </Link>
+        </div>
+      ))
+    )}
+  </div>
+  );  
 };
 
 export default Home;
